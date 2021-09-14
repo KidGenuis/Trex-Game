f2e860dd5a9f3e8979f4dbfef88eb930fca6ef3c
KidGenuis <88520057+KidGenuis@users.noreply.github.com>
2021-09-13 21:46:23 -0400

    Add files via upload

---
+++ Pro-c18-sa1-template_code-main/sketch.js
@@ -0,0 +1,20 @@
+var s1, s1;
+
+function setup() {
+  createCanvas(400,400);
+
+s1 = new Student("leo", 12, 7);
+s1.display();
+
+s2 = new Student("clara", 15, 9);
+s2.display();
+
+}
+
+function draw() 
+{
+  background(30);
+
+  
+  
+}
